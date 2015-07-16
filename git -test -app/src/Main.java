6e51a1f29f6ef3916e47d3c94f45d6642a3be62f
Pathernaxx <kresnik13@gmail.com>
2015-07-16 17:18:03 +0900

    test1

--- git -test -app/src/Main.java
+++ git -test -app/src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		System.out.println("Hello, ");
+		
+		System.out.println("집에 가고 싶다!!!!");
+		
+		System.out.println("한동윤 바보");
 	}
 
 }
